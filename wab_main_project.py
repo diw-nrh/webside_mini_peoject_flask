8699efa54c49d73b9588ef5b12a2b60efefdbe58
wanfadiw <fadiwmimi14@gmail.com>
2025-02-16 21:44:34 +0700

    Add login in file  web_main_project.py

--- wab_main_project.py
+++ wab_main_project.py
@@ -1,8 +1,34 @@
-from flask import Flask , render_template 
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import models
+import forms
 
 app = Flask(__name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = models.User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password!', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/index')
 def index():
     return render_template("index.html")
 
