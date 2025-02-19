4645d6dfcc5ae8d8e164dd2123c54617ccbe0a3e
wanfadiw <fadiwmimi14@gmail.com>
2025-02-19 20:27:17 +0700

    change functoin index to use template index.html

--- wab_main_project.py
+++ wab_main_project.py
@@ -23,7 +23,7 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
